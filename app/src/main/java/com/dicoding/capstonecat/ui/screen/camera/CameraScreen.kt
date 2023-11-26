7dc3d56c45cc9b666816f6623c3a4acc8e19c985
Muhammad Naufal Firdaus <Naufal Firdaus>
2023-11-26 14:34:14 +0700

    nambah galery

--- app/src/main/java/com/dicoding/capstonecat/ui/screen/camera/CameraScreen.kt
+++ app/src/main/java/com/dicoding/capstonecat/ui/screen/camera/CameraScreen.kt
@@ -3,15 +3,22 @@ package com.dicoding.capstonecat.ui.screen.camera
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.FloatingActionButton
@@ -25,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -47,13 +55,19 @@ fun CameraScreen(
 @Composable
 fun imageCaptureFromCamera()
 {
-
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val bitmap = remember { mutableStateOf<Bitmap?>(null) }
     val context = LocalContext.current
     val file = context.createImageFile()
     val uri = FileProvider.getUriForFile(
         Objects.requireNonNull(context),
         context.packageName + ".provider", file
     )
+
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            imageUri = uri
+        }
 
     var capturedImageUri by remember {
         mutableStateOf<Uri>(Uri.EMPTY)
@@ -109,13 +123,55 @@ fun imageCaptureFromCamera()
         }
     }
 
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Bottom,
 
-    if (capturedImageUri.path?.isNotEmpty() == true)
+        ) {
+
+        imageUri?.let {
+            if (Build.VERSION.SDK_INT < 28) {
+                bitmap.value = MediaStore.Images
+                    .Media.getBitmap(context.contentResolver, it)
+            } else {
+                val source = ImageDecoder.createSource(context.contentResolver, it)
+                bitmap.value = ImageDecoder.decodeBitmap(source)
+            }
+
+            bitmap.value?.let { btm ->
+                Image(
+                    bitmap = btm.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(400.dp)
+                        .padding(20.dp)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        FloatingActionButton(
+            onClick = { launcher.launch("image/*") }) {
+            Icon(painter = painterResource(id = R.drawable.ic_image), contentDescription = "galery")
+        }
+    }
+
+
+    if (capturedImageUri.path?.isNotEmpty() == true || imageUri?.path?.isNotEmpty() == true)
     {
         Image(
             modifier = Modifier
                 .padding(16.dp, 8.dp),
             painter = rememberImagePainter(capturedImageUri),
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier
+                .padding(16.dp, 8.dp),
+            painter = rememberImagePainter(imageUri),
             contentDescription = null
         )
     }
